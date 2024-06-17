6feaf2654be8f3d766c4ce36b5a12a1b3e822714
Dan Worley <danworley@gmail.com>
2024-06-17 12:54:53 -0400

    Build a dll instead of executable

--- engine/build.sh
+++ engine/build.sh
@@ -159,7 +159,7 @@ function windows {
 			windres.exe resources/OpenBOR.rc -o resources/OpenBOR.res -O coff
 		fi
     make BUILD_WIN=1
-    if test -f "./OpenBOR.exe"; then
+    if test -f "./OpenBOR.dll"; then
       if test ! -e "./releases/WINDOWS" ; then
         mkdir ./releases/WINDOWS
         mkdir ./releases/WINDOWS/OpenBOR
@@ -168,7 +168,8 @@ function windows {
         mkdir ./releases/WINDOWS/OpenBOR/Saves
         mkdir ./releases/WINDOWS/OpenBOR/ScreenShots
       fi
-      mv OpenBOR.exe ./releases/WINDOWS/OpenBOR
+      mv OpenBOR.lib ./releases/WINDOWS/OpenBOR
+      mv OpenBOR.dll ./releases/WINDOWS/OpenBOR
     fi
     make clean BUILD_WIN=1
   fi
